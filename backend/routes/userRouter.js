a54e790ab9d02e15f1424ef61f8d581a84bfe721
aoropesa-amaoed <arjayoropesa23@gmail.com>
2024-01-27 00:55:22 +0800

    create a contact landlord feature with sending a messgae

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteUser, test, updateUser, getUserListings } from '../controller/userController.js';
+import {deleteUser, test, updateUser, getUserListings, getUser } from '../controller/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 // import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
@@ -12,6 +12,6 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id', verifyToken, getUserListings)
-// router.get('/:id', verifyToken, getUser)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
