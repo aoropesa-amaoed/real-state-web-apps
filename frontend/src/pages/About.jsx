a77f4b47ef08a30da48905bff9fdb2c51978b9dd
aoropesa-amaoed <arjayoropesa23@gmail.com>
2024-01-27 02:30:03 +0800

    complete about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Antelope Estate</h1>
+      <p className='mb-4 text-slate-700'>At Antelope, we understand that finding the perfect property is not just about the physical space; it's about discovering a place where you can build a life, create memories, and call home. As a leading real estate agency, we are dedicated to making your property journey seamless and rewarding.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to empower individuals and families to find their ideal homes or investment properties by providing unparalleled real estate services. We strive to exceed expectations, offering a personalized approach that takes into account your unique needs and aspirations.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
   )
 }
