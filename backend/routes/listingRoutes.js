b55f84e138b8c4fe60e395e03c2246e23b900a95
aoropesa-amaoed <arjayoropesa23@gmail.com>
2024-01-26 23:10:46 +0800

    add delete listing api

--- backend/routes/listingRoutes.js
+++ backend/routes/listingRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createListing, 
-        // deleteListing, 
+        deleteListing, 
         // updateListing, 
         // getListing, 
         // getListings 
@@ -10,7 +10,7 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
-// router.delete('/delete/:id', verifyToken, deleteListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 // router.post('/update/:id', verifyToken, updateListing);
 // router.get('/get/:id', getListing);
 // router.get('/get', getListings);
