99f6b47d2d38b17afe160d7ea39e169f32570619
aoropesa-amaoed <arjayoropesa23@gmail.com>
2024-01-26 21:05:23 +0800

    add delete functionality

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {test, updateUser} from '../controller/userController.js';
+import {deleteUser, test, updateUser} from '../controller/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 // import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
-// router.delete('/delete/:id', verifyToken, deleteUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
 // router.get('/listings/:id', verifyToken, getUserListings)
 // router.get('/:id', verifyToken, getUser)
 
