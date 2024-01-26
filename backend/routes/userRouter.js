f03fe95c90af9327143aeba341a9f97c81c5d03e
aoropesa-amaoed <arjayoropesa23@gmail.com>
2024-01-26 22:46:27 +0800

    getting the listing per user

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {deleteUser, test, updateUser} from '../controller/userController.js';
+import {deleteUser, test, updateUser, getUserListings } from '../controller/userController.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 // import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
-// router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/listings/:id', verifyToken, getUserListings)
 // router.get('/:id', verifyToken, getUser)
 
 export default router;
